e0ef967e148703c60692bf02f8508321d02c035b
Ashish Kumar <85364791+Ashket980@users.noreply.github.com>
2023-02-15 00:46:19 +0530

    Update script.js

--- Task_1/js/script.js
+++ Task_1/js/script.js
@@ -1,1 +1,17 @@
 
+
+function allclear()
+{
+    document.getElementById("res").value="";
+}
+
+function s(input)
+{
+    document.getElementById("res").value+=input;
+}
+
+function res()
+{
+    var out=eval(document.getElementById("res").value);
+    document.getElementById("res").value=out;
+}        
